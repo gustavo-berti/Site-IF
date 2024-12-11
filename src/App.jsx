31f72175fa2f6ba7a1edb1d62e52bd25f0733efe
gustavo-berti <gutigimenez15@gmail.com>
2024-12-10 22:08:57 -0300

    re-add pre-configured project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-function App() {
+export default function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl">Hello, Tailwind CSS in React!</h1>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mt-4 rounded">
-        Click Me
-      </button>
-    </div>
+    <h1 className="text-3xl font-bold underline bg-neutral-950">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
